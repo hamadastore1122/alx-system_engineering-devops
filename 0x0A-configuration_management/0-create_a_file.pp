3d2d0df9eab7259f36136eb61373e660ea5036bf
hamadastore22 <hamadastore22@gmail.com>
2024-03-25 09:50:39 -0700

    hamostart

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# create file in /tmp
+# creates a file in /tmp
 
-file { 'holberton':
-  path    => '/tmp/holberton',
+file { '/tmp/school':
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
 }
